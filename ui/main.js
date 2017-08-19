b7fa135d63fa2923aa47a441a018611fdd2f46b5
KarthikSGKP <30654633+KarthikSGKP@users.noreply.github.com>
2017-08-19 12:47:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,14 @@ button.onclick = function() {
     request.open('GET', "http://karthikpawar52.imad.hasura-app.io/counter", true);
     request.send(null);
 };
+
+// Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+     // Make a list to the server and send the name
+     // Capture a list of names and render it as a list
+    
+    
+};
